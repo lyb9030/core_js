c78138c0eb1a309970602b8e0f5ddf0b265821e9
lyb9030 <lyb_9030@naver.com>
2024-11-06 01:49:09 +0900

    loop-4-2

--- client/chapter/core/10-4.loop.js
+++ client/chapter/core/10-4.loop.js
@@ -28,6 +28,87 @@ console.log(Object.prototype.hasOwnProperty.call(javaScript, 'creator'));
 
 console.log(Object.hasOwn(javaScript, 'nickName'));
 
+// const obj = {};
+
+// const hello = 'a';
+// 123.toLowerCase()
+
+// obj.a()
+
+// in 문은 하나 조회
+
+//for ... in 문은 여러개
+
+let userName = 'tiger';
+let email = 'tiger@naver.com';
+let phone = '010-7169-0262';
+let gender = 'male';
+
+const user1 = {
+  userName: 'tiger',
+  email: '',
+};
+
+// 객체 순환 => email이라는 키가 있는지 확인 => email 안에 빈문자가 들어있는지 ?
+// => 사용자에게 notification
+
 // for ~ in 문
 // - 객체 자신의 속성만 순환하려면?
+
+// 객체의 key를 조회
+// 객체의 value를 조회
+
+for (const key in javaScript) {
+  // if(Object.hasOwn(javaScript,key)){
+  if (Object.prototype.hasOwnProperty.call(javaScript, key)) {
+    const value = javaScript[key];
+
+    // console.log( value );
+  }
+}
+console.clear();
+
 // - 배열 객체 순환에 사용할 경우?
+// 배열도 for...in문으로 순환이 가능은하다.
+// for...in은 객체에 양보하세요
+
+// 배열은 순서에 굉장히 민감한 데이터 => for..in문은 순서를 보장하지 않는다.
+
+const tens = [10, 100, 1000, 10_000];
+
+for (const key in tens) {
+  // console.log(tens[key]);
+}
+
+// enumerable : 열거 가능한
+
+for (const key in javaScript) {
+  console.log(key);
+}
+
+const obj = {};
+
+obj.nickName = 'tiger';
+
+Object.defineProperty(obj, 'age', {
+  value: 30,
+  enumerable: true,
+  writable: false,
+  configurable: false,
+});
+
+Object.defineProperties(obj, {
+  age: {
+    value: 30,
+    enumerable: true,
+    writable: false,
+    configurable: false,
+  },
+  email: {},
+});
+
+console.log(obj);
+
+for (const key in obj) {
+  console.log(key);
+}
